fbbc652fede7b1909e37105cfeee1b15b0cb1608
Beca <becabenevello@gmail.com>
2025-09-18 20:47:27 -0300

    criação de tabela revisão

--- todos/urls.py
+++ todos/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import TodoListView
+from .views import TodoListView,TodoCreatView
 
 urlpatterns = [
     path("", TodoListView.as_view(), name="todo_list"),
+    path("", TodoListView.as_view(), name="todo_list"),
+    path("create", TodoCreatView.as_view(), name="todo_create")
+
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
 from .models import Todo
 
 class TodoListView(ListView):
     model = Todo
     template_name = "todos/todo_list.html"
     context_object_name = "todos"
+
+class TodoCreatView(CreateView):
+    model = Todo
+    fields = ["name", "description", "price"]
+    success_url = reverse_lazy("todo_list")
